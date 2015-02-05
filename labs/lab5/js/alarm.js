4e6aaee03896f8df6dc53c111fd439a9210ad3b2
Gabriel Silva <gabriel.ssilva@outlook.com>
2015-02-05 13:24:05 -0800

    Adding Lab 5

---
+++ labs/lab5/js/alarm.js
@@ -0,0 +1,104 @@
+function showAlarmPopup() {
+	$("#mask").removeClass("hide");
+	$("#popup").removeClass("hide");
+}
+
+function hideAlarmPopup() {
+	$("#mask").addClass("hide");
+	$("#popup").addClass("hide");
+}
+
+function addAlarm() {
+	var hours = $("#hours option:selected").text();
+	var mins = $("#mins option:selected").text();
+	var ampm = $("#ampm option:selected").text();
+	var time = hours+":"+mins+" "+ampm;
+	var alarmName = $("#alarmName").val();
+
+	Parse.initialize("<private>", "<private>");
+	var AlarmObject = Parse.Object.extend("Alarm");
+	var newAlarm = new AlarmObject();
+	newAlarm.save({ "time": time, "name": alarmName }, { success: function(object) {
+			getAllAlarms();
+			hideAlarmPopup();
+		}
+	});
+}
+
+function insertAlarm(id, time, alarmName) {
+	var newDiv = $("<div></div>");
+	newDiv.addClass("flexable");
+	newDiv.addClass("alarm-entry");
+
+	var nameDiv = $("<div></div>");
+	nameDiv.addClass("name");
+	nameDiv.html(alarmName);
+
+	var timeDiv = $("<div></div>");
+	timeDiv.addClass("time");
+	timeDiv.html(time);
+
+	var button = $("<input/>");
+	button.addClass("remove-button");
+	button.attr("type", "button");
+	button.attr("value", "X");
+	button.attr("id", id);
+
+	newDiv.append(nameDiv);
+	newDiv.append(timeDiv);
+	newDiv.append(button);
+
+	$("#alarms").append(newDiv);
+}
+
+function removeAlarm(alarmId) {
+	Parse.initialize("<private>", "<private>");
+
+	var AlarmObject = Parse.Object.extend("Alarm");
+	var query = new Parse.Query(AlarmObject);
+	query.get(alarmId, {
+		success: function(result) {
+			result.destroy({
+				success: function() {
+					getAllAlarms();
+				}
+			});
+		}
+	});
+}
+
+function getAllAlarms() {
+	$("#alarms").html("");
+
+	Parse.initialize("<private>", "<private>");
+	var AlarmObject = Parse.Object.extend("Alarm");
+	var query = new Parse.Query(AlarmObject);
+	query.find({
+		success: function(results) {
+			if (results.length == 0) {
+				$("#alarm-header-title").html("No Alarms set");
+			} else {
+				$("#alarm-header-title").html("Alarms");
+			}
+
+			for(var i=0; i<results.length; i++) {
+				insertAlarm(results[i].id, results[i].get("time"), results[i].get("name"));
+			}
+		}
+	});
+}
+
+
+function init() {
+	$("#add-btn").click(showAlarmPopup);
+	$("#cancel-btn").click(hideAlarmPopup);
+	$("#set-btn").click(addAlarm);
+
+	$('#alarms').on('click', '.remove-button', function() {
+    	removeAlarm(this.id);
+	});
+
+	getAllAlarms();
+}
+
+$(document).onLoad = init();
---
+++ labs/lab5/js/clock.js
@@ -0,0 +1,81 @@
+var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Tuesday", "Friday", "Saturday"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", 
+			  "September", "October", "November", "December"];
+
+function getTime() {
+	var datetime = new Date();
+	var hours = ("0" + datetime.getHours()).slice(-2);
+	var minutes = ("0" + datetime.getMinutes()).slice(-2);
+
+	$("#time").html(hours + ":" + minutes);
+	setTimeout(getTime, 1000);
+}
+
+function setBodyClass(temp) {
+	var body;
+
+	if (temp >= 90) {
+		body = "hot";
+	} else if (temp >= 80) {
+		body = "warm";
+	} else if (temp >= 70) {
+		body = "nice";
+	} else if (temp >= 60) {
+		body = "chilly";
+	} else{
+		body = "cold";
+	}
+
+	$("body").attr("class", body);
+	$(".btn").attr("class", "btn btn-primary text-center "+body);
+}
+
+function getTemp(response, statusR, xhrR) {
+	var key = "<private>";
+	var position = response.loc;
+	var city = response.city;
+	var region = response.region;
+	var url = "https://api.forecast.io/forecast/"+key+"/"+position+"?callback=?";
+
+	$.getJSON(url, function(data, status, xhr) {
+		var icon = "img/"+data.currently.icon+".png";
+		var summary = data.currently.summary;
+		var temperature = Math.round(data.currently.temperature);
+
+		var datetime = new Date(data.currently.time * 1000);
+		var date = weekdays[datetime.getDay()] + ", " + months[datetime.getMonth()] + " " + datetime.getDate();
+
+		setBodyClass(temperature);
+		$(".forecast-label.big").html(summary);
+		$(".forecast-icon.big").attr("src", icon);
+		$(".forecast-temp").html(temperature+"&deg;F");
+		$("#date").html(date);
+		$("#location").html(city + ", " + region);
+		getWeekTemp(data);
+	});
+}
+
+function getLocation() {
+	$.getJSON("http://ipinfo.io/json", getTemp);
+}
+
+function getWeekTemp(data) {
+	for(var i=1; i<=5; i++) {
+		var datetime = new Date(data.daily.data[i].time * 1000);
+		var day = weekdays[datetime.getDay()];
+		var icon = "img/"+data.daily.data[i].icon+".png";
+		var temperatureMin = Math.round(data.daily.data[i].temperatureMin);
+		var temperatureMax = Math.round(data.daily.data[i].temperatureMax);
+
+
+		$(".forecast-icon.small.day"+i).attr("src", icon);
+		$(".forecast-label.small.day"+i).html(day+ ": " + temperatureMin+"/"+temperatureMax+" &deg;F");
+	}
+}
+
+function init() {
+	getTime();
+	getLocation();
+}
+
+$(document).onLoad = init();
